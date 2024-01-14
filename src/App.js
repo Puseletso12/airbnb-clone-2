04dfe6dd12bd481ec8140e2d9e93004c5cff4c8f
Puseletso Chuene <puseletsochuene88@gmail.com>
2024-01-14 18:43:14 +0200

    date picker feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/layout/Header';
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <div className='app'>
       <Header />
+      <Home />
       {/* 
       Home Page
         Header
